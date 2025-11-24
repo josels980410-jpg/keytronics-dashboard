ca7693edb80cbb03330db3e82b0059bd47778a1f
Jose Luna <josels980410@gmail.com>
2025-11-24 10:12:27 -0600

    Se actualiza URL

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Para actualizar cualquier modificación, en nuestra terminal tenemos que correr 
 los siguientes comandos: 
 
 1.-    git add .
-2.-    git commit -m "Actualización de estilos o textos"
+2.-    git commit -m "Se actualiza URL"
 3.-    git push origin master
 
 Nota: 
--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def dashboard():
     if "user" not in session:
         return redirect(url_for("home"))
 
-    embed_url = "https://app.powerbi.com/view?r=eyJrIjoiZTEyNTJjZTUtMjg0ZC00ZDVmLTlhYmMtMDc3ODIyY2Y3Njk2IiwidCI6IjAzODk5MTIxLWQ5NzYtNDRlOS1iODI0LTFmYzU1N2JmZGRjZSJ9"
+    embed_url = "https://app.powerbi.com/view?r=eyJrIjoiZDUxOGFlMjUtM2U0Ny00OWUzLWFlOTEtMTIzYzNjZjkzZjUxIiwidCI6IjAzODk5MTIxLWQ5NzYtNDRlOS1iODI0LTFmYzU1N2JmZGRjZSJ9"
 
     return f"""
     <html>
